36e1a69997c7f67f861aaccb73f337eae168fbbb
Sean Luo <41530060+lu0x1a0@users.noreply.github.com>
2021-12-21 14:45:11 +1100

    in progress to move all mcmc to pytorch

---
+++ myLGMCMC.py
@@ -0,0 +1,136 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.transforms as transforms
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import time
+from scipy.stats import multivariate_normal
+from scipy.stats import norm
+import math
+import os
+class FC(nn.Module):
+    # replica of Dr Chandra's code
+    def __init__(self,layer_shapes,activation = nn.Sigmoid, out_act = nn.Sigmoid):
+        super().__init__()
+        self.topology = layer_shapes
+        self.layerslist = []
+        for i,x in enumerate(self.topology):
+            if i != (len(self.topology)-1):
+                self.layerslist.append(nn.Linear(*x))
+                self.layerslist.append(activation())
+            else:
+                self.layerslist.append(nn.Linear(*x))
+                self.layerslist.append(out_act())
+        self.nn = nn.Sequential(*self.layerslist)
+
+    def forward(self,x):
+        return self.nn(x)
+    def encode(self):
+        return torch.cat([x.flatten() for x in self.parameters()])
+    def decode(self,w):
+        return 
+class MCMC:
+    def __init__(self,trainx,trainy,testx,testy, use_langevin,langevin_prob,learning_rate,n_full_batches,networktype = 'fc', hidden_size = [5] ):
+        """ assumes that y is normally distributed with mean represented by nn model:
+            unknowns:
+                tau - sd of y
+                w - weights and bias 
+            hyperparameters - a,b, Sigma
+
+            # in Dr Chandra's paper, theta = (weight,bias,tau (y sample variance) )
+            # here it's different
+
+            p(y_S|w,tau) = 1/(2pi tau^2)^(S/2) * exp(-(sum (y_t - f(x_t))^2)/(2tau^2))
+            p(w1,...wn) ~ N(0_, Sigma_) , Sigma = covar matrix
+            p(w1,...wn) ~ 1/(2pi sigma^2)^n exp(()/2)
+            tau^2 ~ IGamma(a,b) => 1/tau ~ Gamma(a,b)
+            now we want to simulate p(w|x,y,tau)
+            mcmc steps:
+                # i want to use gibb sample for tau_i|w_i-1,y,x
+                # the code by Dr Chandra uses normal proposal for tau instead that might be easier to evaluate.
+                # both proposal distribution by R.C uses a step for std. 
+
+                propose w|tau,y,x from N(w_ld,)
+                accept-reject
+            
+        Args:
+            trainx ([ndarray]): [description]
+            trainy ([ndarray]): [description]
+            testx ([ndarray]): [description]
+            testy ([ndarray]): [description]
+            use_langevin ([bool]): [description]
+            langevin_prob ([float32:[0,1])]): [description]
+            learning_rate ([float32]): [description]
+            n_full_batches ([int]): [description]
+            networktype (str, optional): [description]. Defaults to 'fc'.
+            hidden_size (list, optional): [not including input layer(data) size and output layer size]. Defaults to [5].
+        """
+
+        self.trainx = trainx if isinstance(trainx,torch.Tensor) else torch.from_numpy(trainx)
+        self.trainy = trainy if isinstance(trainy,torch.Tensor) else torch.from_numpy(trainy)
+        self.testx  = testx  if isinstance(testx,torch.Tensor)  else torch.from_numpy(testx)
+        self.testy  = testy  if isinstance(testy,torch.Tensor)  else torch.from_numpy(testy)
+        self.use_langevin = use_langevin
+        self.langevin_prob = langevin_prob
+        self.lr = learning_rate
+        self.n_iter = n_full_batches
+
+        self.networktype = networktype
+        if networktype == 'fc':            
+            self.network_shape = []
+            self.n_weights = 0
+            for i,x in enumerate(hidden_size):
+                if i == 0:
+                    self.network_shape.append((trainx.shape[-1],x))
+                    self.n_weights += trainx.shape[-1]*x+x
+                else:
+                    self.network_shape.append((hidden_size[i-1],x))
+                    self.n_weights += hidden_size[i-1]*x+x
+                if i == (len(hidden_size)-1):
+                    shape = (x,1 if testy.ndim == 1 else testy.shape[-1] )
+                    self.network_shape.append(shape)
+                    self.n_weights += shape[0]*shape[1]+shape[1]
+            self.network = FC(self.network_shape)
+            
+            self.loss = nn.MSELoss()
+    def p_theta(self): # p(theta)
+        ...
+    def p_theta_x(self): # p(theta|X)
+        ...
+    def p_x_theta(self): # p(X|theta)
+        ...
+    def proposal(self): # p(theta*|theta_t)
+        ...
+    def randomwalkproposal(self):
+        ...
+    def sample(self, a,b,sigma2):
+        """[summary]
+
+        Args:
+            a ([type]): [param for tau prior]
+            b ([type]): [param for tau prior]
+            sigma2 ([type]): [param for w prior]
+        """
+        # uses default torch init for weights and sample variance as starter (i.e. null model)
+        predY = self.network(self.trainx)
+        tau2 = np.zeros(self.n_iter)
+        tau2[0] = np.var(self.trainy) #np.random.rand(np.var(self.trainy))+0.00001
+
+        tau_posterior_a = self.n_weights + a + 1
+        for i in range(1,self.n_iter+1):
+            tau_posterior_b = b + sum((self.trainy-predY)**2)/2
+            tau2[i] = 1/torch.Gamma(tau_posterior_a,tau_posterior_b).sample()
+from torchinfo import summary
+if __name__ == '__main__':
+    mcmc = MCMC(np.zeros((100,5)),np.zeros((100,1)),np.zeros((100,5)),np.zeros((100,1)),True,1,0.01,1000,networktype='fc',hidden_size=[8,5,2])
+    print(mcmc.network)
+    batch_size = 10
+    summary(mcmc.network,input_size=(batch_size,5))
+
+    
